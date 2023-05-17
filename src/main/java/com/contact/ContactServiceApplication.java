93d85f8ceb05be9eb9ed5ab84b8fd7e0575e2690
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-17 16:54:42 +0530

    Changed pom and main so that it can generate war

--- src/main/java/com/contact/ContactServiceApplication.java
+++ src/main/java/com/contact/ContactServiceApplication.java
@@ -1,13 +1,20 @@
 package com.contact;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactServiceApplication {
+public class ContactServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactServiceApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		return application.sources(ContactServiceApplication.class);
 	}
 
 }
