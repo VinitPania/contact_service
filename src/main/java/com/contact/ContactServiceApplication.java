6355ef9029ac68c7f97c30f660bf8b02542d3b09
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-25 15:47:31 +0530

    Removed cloud config serever

--- src/main/java/com/contact/ContactServiceApplication.java
+++ src/main/java/com/contact/ContactServiceApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.config.server.EnableConfigServer;
+// import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
-@EnableConfigServer
+// @EnableConfigServer
 public class ContactServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
