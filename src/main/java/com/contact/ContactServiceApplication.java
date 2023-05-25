6abeb128a687dfde4a48e9e7e1e5c13103d00d38
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-25 17:28:10 +0530

    changes the application.properties and changed port no

--- src/main/java/com/contact/ContactServiceApplication.java
+++ src/main/java/com/contact/ContactServiceApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 // import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-// @EnableConfigServer
+@EnableDiscoveryClient
 public class ContactServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
